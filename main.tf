4f6d9a209b4b9d3a65950e255dc98ee183e72b4d
Jonathan Vednyonga <jonathanokon98@gmail.com>
2024-02-19 20:22:11 +0000

    Added a second version

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
   access_key = var.access-key
@@ -13,85 +14,77 @@ provider "aws" {
 }
 
 variable "access-key" {
-  description = "access key"
+  description = "My account access key"
   type        = string
 }
-
 variable "secret-key" {
-  description = "secret access key"
+  description = "My account secret key"
   type        = string
 }
 
-# vpc
-resource "aws_vpc" "dev-vpc" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# How to Create a resource
+#resource "<provider>_<resource_type>" "name" {
+#  config options
+#}
 
+# To begin: Create a Key Pair on AWS
+# 1. Create a VPC
+resource "aws_vpc" "VPC" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Dev-VPC"
-
+    Name = "Lab-VPC"
   }
 }
+# 2. Create an Internet gateway 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.VPC.id
 
-# subnet (public)
-resource "aws_subnet" "dev_public_subnet" {
-  vpc_id                  = aws_vpc.dev-vpc.id
-  cidr_block              = "10.123.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+}
+
+# 3. Create a Route Table
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "Dev-Public-Subnet1"
+    Name = "Lab-route"
   }
 }
-
-# internet gateway 
-resource "aws_internet_gateway" "dev_igw" {
-  vpc_id = aws_vpc.dev-vpc.id
+# 4. Create a Subnet 
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.VPC.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "dev-igw"
+    Name = "Lab-subnet-1"
   }
 }
-
-# route table (table, route & association)
-resource "aws_route_table" "dev_rt" {
-  vpc_id = aws_vpc.dev-vpc.id
-
-  tags = {
-    Name = "dev-route"
-  }
+# 5. Associate subnet to Route table 
+resource "aws_route_table_association" "route" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.route-table.id
 }
-
-resource "aws_route" "dev_route" {
-  route_table_id         = aws_route_table.dev_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.dev_igw.id
-
-}
-
-resource "aws_route_table_association" "dev_a" {
-  subnet_id      = aws_subnet.dev_public_subnet.id
-  route_table_id = aws_route_table.dev_rt.id
-}
-
-# security group
-resource "aws_security_group" "dev_sg" {
-  name        = "dev-sg"
-  description = "dev security group"
-  vpc_id      = aws_vpc.dev-vpc.id
+# 6. Create a Security group to allow ports 22, 80, 443
+resource "aws_security_group" "WebSG" {
+  name        = "WebSG"
+  description = "Allow SSH, HTTP and HTTPS traffic"
+  vpc_id      = aws_vpc.VPC.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -110,18 +103,57 @@ resource "aws_security_group" "dev_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Name = "WebSG"
+  }
 }
 
-# instance
-resource "aws_instance" "dev_instance" {
-  ami                    = "ami-06aa3f7caf3a30282"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
-  key_name               = "ec2-keypair"
-  vpc_security_group_ids = [aws_security_group.dev_sg.id]
-  subnet_id              = aws_subnet.dev_public_subnet.id
-  user_data              = file("userdata.tpl")
-  tags = {
-    Name = "dev-server"
+# 7. Create a network interface (ENI) with an ip in the subnet that was created (private ip for web server)
+resource "aws_network_interface" "Web-ENI" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.WebSG.id]
+
+}
+
+# 8. Create an Linux server and install/enable apache 2
+
+resource "aws_instance" "WebServer1" {
+  ami               = "ami-0cf10cdf9fcd62d37"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "ec2-keypair"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.Web-ENI.id
   }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update
+                yum -y install httpd
+                chkconfig httpd on
+                service httpd start
+                EOF
+
+  tags = {
+    Name = "Web-Server"
+  }
+}
+
+# 9. Assaign an elastic IP to the network interface (public ip for web server)
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.Web-ENI.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.igw, aws_instance.WebServer1]
+}
+
+output "server_public_ip" {
+  value = aws_instance.WebServer1.public_ip
+}
+
+output "server_elastic_ip" {
+  value = aws_eip.one.public_ip
 }
